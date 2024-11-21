48e83c6ddae1bb8ca7e1397b67b2eb7dca3a93d6
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-11-21 19:45:49 -0300

    Implementando validação do arquivo csv no app datapanel

--- datapanel/views.py
+++ datapanel/views.py
@@ -1,8 +1,34 @@
+from shutil import rmtree
+import pandas as pd
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 
 @login_required(login_url='login')
 def data_panel(request):
     if request.method == 'GET':
         return render(request, 'data_panel/data_panel.html')
+
+    # Apagando o diretório client_data e seu conteúdo
+    rmtree(settings.MEDIA_ROOT, ignore_errors=True)
+
+    # Obtendo o novo arquivo enviado no formulário
+    dataset_file = request.FILES['file']
+
+    # Salvando a entrada fisicamente no sistema de arquivos e obtendo seu nome
+    fs = FileSystemStorage()
+    filename = fs.save(dataset_file.name, dataset_file)
+
+    # Lendo o arquivo de dataset padrão
+    df = pd.read_csv(settings.DEF_DATASET, sep=",")
+
+    # Lendo o arquivo de dataset recebido
+    file_path = settings.MEDIA_ROOT / filename
+
+    df_input = pd.read_csv(file_path, sep=',')
+
+    if not df.columns.equals(df_input.columns):
+        return render(request, 'data_panel/data_panel.html', {'invalidCSV': True})
+    return render(request, 'data_panel/data_panel.html')
